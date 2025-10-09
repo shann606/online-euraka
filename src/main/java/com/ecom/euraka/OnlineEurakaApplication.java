b60205f32f8ec840d2484cdfaf947680c5e775c4
shann606 <shann606@gmail.com>
2025-10-09 22:43:02 +0530

    first commit

---
+++ src/main/java/com/ecom/euraka/OnlineEurakaApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.euraka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEurakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEurakaApplication.class, args);
+	}
+
+}
